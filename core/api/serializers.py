90633c3fcf72ce19c627ad51e73a8a443e2a5627
yukito <iyggf66974@yahoo.ne.jp>
2025-03-15 14:30:45 +0900

    loginPage 成功

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from ..models import Post, Job
+from django.contrib.auth.models import User
+from ..models import UserProfile
 
 # class DeviceSerializer(serializers.ModelSerializer):
 #     class Meta:
@@ -10,3 +12,29 @@ class DeviceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         fields = ['id', 'company', 'place', 'status']
+
+
+
+
+
+# serializers.py
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = UserProfile
+        fields = ('user',)
--- core/api/urls.py
+++ core/api/urls.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from django.urls import path
-
+from .views import UserRegistrationView, UserLoginView, UserProfileView
 from . import views
 
 urlpatterns = [
@@ -8,5 +8,10 @@ urlpatterns = [
   path('device/<int:pk>/', views.TaskRetrieveUpdataDestroyAPIView.as_view()),
   path('jobs/', views.JobsPage),
   path('jobs-create/', views.CreateJobsPage),
+
+  # User Login
+  path('register/', UserRegistrationView.as_view(), name='register'),
+  path('login/', UserLoginView.as_view(), name='login'),
+  path('profile/', UserProfileView.as_view(), name='profile'),
 
 ]
